a2d3632141b78ce8fdda3be58c6e986e3a66a354
Sunaina <mohammadsunaina9@gmail.com>
2026-03-10 22:05:02 +0530

    First Commit

---
+++ practice.py
@@ -0,0 +1,7 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.debug("Debug message")
+logging.info("I started learning git")
+logging.warning("Low disks space")
+logging.error("Something went wrong")
+logging.critical("system Crash")
