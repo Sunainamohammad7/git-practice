558563afedd1db0fdb3888eef8539c286fc49dc2
Sunaina <mohammadsunaina9@gmail.com>
2026-03-12 13:02:42 +0530

    updated python file

--- practice.py
+++ practice.py
@@ -1,6 +1,6 @@
 import logging
 logging.basicConfig(level=logging.INFO,format="%(asctime)s-%(levelname)s-%(message)s")
-logging.debug("Debug message")
+logging.debug("Debug messages")
 logging.info("I started learning git")
 logging.warning("Low disks space")
 logging.error("Something went wrong")
